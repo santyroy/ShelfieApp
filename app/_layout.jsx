504b1790e5e49dedf81d1d8979e8bf4347f386dc
Sougata Roy <sougata.roy26@yahoo.com>
2025-04-29 18:33:43 +0530

    Light and Dark themes

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,22 +1,24 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import { useColorScheme } from "react-native";
+import { Colors } from "../constants/Colors";
 
 const RootLayout = () => {
+  const colorScheme = useColorScheme();
+  const theme = Colors[colorScheme ?? "light"];
+
   return (
     <>
       <StatusBar style="auto" />
       <Stack
         screenOptions={{
-          headerStyle: { backgroundColor: "#ddd" },
-          headerTintColor: "#333",
+          headerStyle: { backgroundColor: theme.navBackground },
+          headerTintColor: theme.title,
         }}
       >
         <Stack.Screen name="index" options={{ title: "Home" }} />
         <Stack.Screen name="about" options={{ title: "About" }} />
-        <Stack.Screen
-          name="contact"
-          options={{ title: "Contact", headerShown: false }}
-        />
+        <Stack.Screen name="contact" options={{ title: "Contact" }} />
       </Stack>
     </>
   );
