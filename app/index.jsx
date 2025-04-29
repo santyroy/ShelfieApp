cb7609fb93ffd0d1d82ec5322cb9f6a2dfdbfbab
Sougata Roy <sougata.roy26@yahoo.com>
2025-04-29 17:49:46 +0530

    Text, View and Image components

--- app/index.jsx
+++ app/index.jsx
@@ -1,13 +1,32 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 
 const Home = () => {
+  const logo = require("../assets/img/logo_light.png");
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Image source={logo} style={styles.img} />
+
+      <Text style={styles.title}>The Number 1</Text>
+
+      <Text style={{ marginTop: 10, marginBottom: 30 }}>Reading List App</Text>
     </View>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontWeight: "900",
+    fontSize: 18,
+  },
+  img: {
+    marginVertical: 20,
+  },
+});
