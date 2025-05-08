422ca8e9c2971222418c781cf560280555e6ced7
Sougata Roy <sougata.roy26@yahoo.com>
2025-05-08 22:31:03 +0530

    Initial Auth State

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -7,12 +7,12 @@ import ThemedButton from "../../components/ThemedButton";
 import Spacer from "../../components/Spacer";
 
 const Profile = () => {
-  const { logout } = useUser();
+  const { logout, user } = useUser();
 
   return (
     <ThemedView style={styles.container}>
       <ThemedText title={true} style={styles.heading}>
-        Your Email
+        {user?.email}
       </ThemedText>
       <Spacer />
       <ThemedText>Time to start reading some books...</ThemedText>
--- context/UserContext.jsx
+++ context/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { account } from "../lib/appwrite";
 import { ID } from "react-native-appwrite";
 
@@ -6,6 +6,7 @@ export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [authChecked, setAuthChecked] = useState(false);
 
   const login = async (email, password) => {
     try {
@@ -31,8 +32,25 @@ export const UserProvider = ({ children }) => {
     setUser(null);
   };
 
+  const getInitailUserDetails = async () => {
+    try {
+      const response = await account.get();
+      setUser(response);
+    } catch (error) {
+      setUser(null);
+    } finally {
+      setAuthChecked(true);
+    }
+  };
+
+  useEffect(() => {
+    getInitailUserDetails();
+  }, []);
+
   return (
-    <UserContext.Provider value={{ user, login, register, logout }}>
+    <UserContext.Provider
+      value={{ user, login, register, logout, authChecked }}
+    >
       {children}
     </UserContext.Provider>
   );
