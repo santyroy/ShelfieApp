4b23d9df2525364c0f7d6bb0bd244723ed04ec6f
Sougata Roy <sougata.roy26@yahoo.com>
2025-05-14 20:12:28 +0530

    Update useBooks to named export

--- hooks/useBooks.jsx
+++ hooks/useBooks.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { BooksContext } from "../context/BooksContext";
 
-export default function useBooks() {
+export function useBooks() {
   const context = useContext(BooksContext);
   if (!context) {
     throw new Error("useBooks must be used within a BooksProvider");
